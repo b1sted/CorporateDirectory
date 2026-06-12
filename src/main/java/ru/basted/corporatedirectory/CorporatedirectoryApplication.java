4cbb86ada8261ae7695a33cc6efb4fb18e0963e0
Nikita Mandrykin <189896017+b1sted@users.noreply.github.com>
2026-06-12 18:49:59 +0300

    Initial commit

---
+++ src/main/java/ru/basted/corporatedirectory/CorporatedirectoryApplication.java
@@ -0,0 +1,13 @@
+package ru.basted.corporatedirectory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CorporatedirectoryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CorporatedirectoryApplication.class, args);
+	}
+
+}
